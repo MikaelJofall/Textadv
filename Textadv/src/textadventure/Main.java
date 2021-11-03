97e4a27761e9466b2d2ca74095177db58f0417db
MikaelJofall <90826121+MikaelJofall@users.noreply.github.com>
2021-11-03 18:29:17 +0100

    Add files via upload

---
+++ Textadv/src/textadventure/Main.java
@@ -0,0 +1,11 @@
+package textadventure;
+
+public class Main {
+    public static void main(String[] args) {
+        TextAdventure tag = new TextAdventure();
+
+        tag.initialization();
+        tag.runGame();
+        tag.quit();
+    }
+}
